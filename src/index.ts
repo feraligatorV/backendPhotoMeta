a929fef00d84341b102d3bae5788c066459bdc81
Fernando <fvelasquez@easygosa.net>
2024-07-10 03:05:44 -0600

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { getPhoto, getFilteredPh } from "./controllers/photoController";
+
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(cors());
 
 app.get('/externalapi/photos/:id', getPhoto);
 app.get('/externalapi/photos', getFilteredPh);
