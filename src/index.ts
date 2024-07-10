7a1088c358484491a0adf9cc6c422220307d6bc4
Fernando <fvelasquez@easygosa.net>
2024-07-10 00:08:53 -0600

    deleted test ci/cd

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,7 @@ const port = process.env.PORT || 3000;
 
 app.get('/externalapi/photos/:id', getPhoto);
 app.get('/externalapi/photos', getFilteredPh);
-app.get('/test', (req,res)=>{
-    res.send('prueba')
-});
+
 
 app.listen(port, ()=> {
     console.log(`server is running on http://localhost:${port}`);
